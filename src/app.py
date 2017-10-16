dfef1707dbca3a3f28cc4ac5b1176a8eaa82ca14
JuLima14 <julian-lima@live.com>
2017-10-16 20:22:46 -0300

    get the port from heroku server

--- src/app.py
+++ src/app.py
@@ -8,6 +8,6 @@ from connection import Connection
 
 if __name__ == "__main__":
     print('Welcome! listening on port {}'.format(PORT))
-
-    Application([(r"/", Connection,)]).listen(PORT)
+    port = int(os.environ.get("PORT", 5000))
+    Application([(r"/", Connection,)]).listen(port)
     ioloop.IOLoop.instance().start()
