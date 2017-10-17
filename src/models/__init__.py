58008c5832276251cf52eceb3c241a82b5190fe3
JuLima14 <julian-lima@live.com>
2017-10-16 21:08:39 -0300

    changes

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,10 +1,8 @@
-import sqlalchemy
-import sqlalchemy.orm
-import sqlalchemy.ext.declarative
 
-#from sqlalchemy import create_engine
-#from sqlalchemy.orm import sessionmaker
-#from sqlalchemy.ext.declarative import declarative_base
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
