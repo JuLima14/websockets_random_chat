405e9d77fc84c8ef15468dd723095d8a3e42c0cb
JuLima14 <julian-lima@live.com>
2017-10-16 21:06:36 -0300

    modify the initialization of database

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,20 +1,29 @@
+import sqlalchemy
+import sqlalchemy.orm
+import sqlalchemy.ext.declarative
+
 #from sqlalchemy import create_engine
 #from sqlalchemy.orm import sessionmaker
 #from sqlalchemy.ext.declarative import declarative_base
 
-#Base = declarative_base()
+Base = declarative_base()
 
-#engine = create_engine('sqlite:///database.db')
-#Base.metadata.bind = engine
-#DBSession = sessionmaker(bind=engine)
-#session = DBSession()
+engine = create_engine('sqlite:///database.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 #from user import User
 #from chat import Chat
 #from membership import Membership
 #from message import Message
 
-#Base.metadata.create_all(engine, checkfirst=True)
+import user 
+import chat
+import membership
+import message
+
+Base.metadata.create_all(engine, checkfirst=True)
 
 
 def get_or_create(session, model, defaults, **kwargs):
