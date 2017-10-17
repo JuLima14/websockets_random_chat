524acbac68cb0b82ee6e7bd7982580907d02fac4
JuLima14 <julian-lima@live.com>
2017-10-16 22:13:08 -0300

    changes

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -11,7 +11,7 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-import *
+from models import *
 #from chat import Chat
 #from membership import Membership
 #from message import Message
