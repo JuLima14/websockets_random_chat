54de8664fe25479cff5326f5df5f440488091f62
JuLima14 <julian-lima@live.com>
2017-10-16 21:14:24 -0300

    changes

--- src/app.py
+++ src/app.py
@@ -11,6 +11,5 @@ from connection import Connection
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     print('Welcome! listening on port {}'.format(port))
-    print('Welcome! listening on port {}'.format($PORT))
     Application([(r"/", Connection,)]).listen(port)
     ioloop.IOLoop.instance().start()
